aa344d2d57d76b8918370517ca5620a8bb6c42c9
Alpha-v2 <96711714+Alpha-v2@users.noreply.github.com>
2023-11-14 16:47:37 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,14 +4,14 @@ Pipeline{
       agent{label "build_server"}
       Stages{
       // Creating build job
-        Stage(Building Maven War){
+        Stage("Building Maven War"){
           Steps{
              sh "mvn clean install"
 
           }
 
         }
-        Stage(Copy War to s3 bucket){
+        Stage("Copy War to s3 bucket"){
 
         Steps{
 
