c167d8570ec973021b56a118734aeebd5a63c106
Alpha-v2 <96711714+Alpha-v2@users.noreply.github.com>
2023-11-14 16:50:04 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,19 +1,19 @@
 def s3_source = "/home/ubuntu/FirstProject/target/FirstProject.war"
 
-Pipeline{
-      agent{label "build_server"}
-      Stages{
+Pipeline {
+      agent {label "build_server"}
+      Stages {
       // Creating build job
-        Stage("Building Maven War"){
-          Steps{
+        Stage ("Building Maven War") {
+          Steps {
              sh "mvn clean install"
 
           }
 
         }
-        Stage("Copy War to s3 bucket"){
+        Stage ("Copy War to s3 bucket") {
 
-        Steps{
+        Steps {
 
         sh "s3 cp ${s3_source} s3://cicd.demo"
         }
